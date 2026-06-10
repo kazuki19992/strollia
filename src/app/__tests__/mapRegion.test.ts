5006460be86151196c7981155840fae9deb83db7
kazuki19992 <kushida98@gmail.com>
2026-06-11 01:00:06 +0900

    test(map): Region検証の回帰テストを追加

--- src/app/__tests__/mapRegion.test.ts
+++ src/app/__tests__/mapRegion.test.ts
@@ -25,4 +25,21 @@ describe('地図表示範囲 mapRegion', () => {
     expect(isValidMapCoordinate({ latitude: 35.681236, longitude: 181 })).toBe(false);
     expect(isValidMapCoordinate({ latitude: 35.681236, longitude: 139.767125 })).toBe(true);
   });
+
+  test('不正な現在地座標ではRegionを作らない', () => {
+    expect(() => createUserCenteredRegion({ latitude: Number.NaN, longitude: 139.767125 })).toThrow();
+    expect(() => createUserCenteredRegion({ latitude: 35.681236, longitude: Number.POSITIVE_INFINITY })).toThrow();
+    expect(() => createUserCenteredRegion({ latitude: 91, longitude: 139.767125 })).toThrow();
+    expect(() => createUserCenteredRegion({ latitude: 35.681236, longitude: 181 })).toThrow();
+  });
+
+  test('有効な現在地座標ではRegionを作る', () => {
+    expect(() => createUserCenteredRegion({ latitude: 35.681236, longitude: 139.767125 })).not.toThrow();
+    expect(createUserCenteredRegion({ latitude: 35.681236, longitude: 139.767125 })).toEqual({
+      latitude: 35.681236,
+      longitude: 139.767125,
+      latitudeDelta: USER_LOCATION_REGION_DELTA,
+      longitudeDelta: USER_LOCATION_REGION_DELTA,
+    });
+  });
 });
